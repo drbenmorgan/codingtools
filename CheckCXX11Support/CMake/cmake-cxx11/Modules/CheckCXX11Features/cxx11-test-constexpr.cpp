8f8f035c42370d445c7ef482e944934dbd181480
Ben Morgan <bmorgan.warwick@gmail.com>
2014-03-24 10:43:42 +0000

    Add constexpr variable check
    Assign a constexpr variable in general constexpr test program.
    Link this to FNAL cpp0x's separate definitions for constexpr functions
    and variables. If one is present the other should be.

--- CheckCXX11Support/CMake/cmake-cxx11/Modules/CheckCXX11Features/cxx11-test-constexpr.cpp
+++ CheckCXX11Support/CMake/cmake-cxx11/Modules/CheckCXX11Features/cxx11-test-constexpr.cpp
@@ -10,10 +10,12 @@ constexpr int the_answer()
 
 int main()
 {
+  constexpr int cVar = 42;
 	int test_arr[square(3)];
 	bool ret = (
 		(square(the_answer()) == 1764) &&
-		(sizeof(test_arr)/sizeof(test_arr[0]) == 9)
+		(sizeof(test_arr)/sizeof(test_arr[0]) == 9) &&
+    (cVar == the_answer())
 	);
 	return ret ? 0 : 1;
 }
