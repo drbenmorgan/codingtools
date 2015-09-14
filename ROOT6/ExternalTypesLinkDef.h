d0005916d2f5d54a033451083ad476a1e74f0bfe
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-09-14 17:38:13 +0100

    Try and generate dictionaries for Boost types
    NB this looks to not work, so this commit is for reference! Rootcling
    segfaults on try to generate dictionaries for boost::variant.
    Likely that this cannot be used...

---
+++ ROOT6/ExternalTypesLinkDef.h
@@ -0,0 +1,9 @@
+
+#ifdef __ROOTCLING__
+#pragma link C++ namespace boost;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class boost::variant<int,double>;
+
+#endif
